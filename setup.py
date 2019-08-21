df795d8c5531087c32242cc956a9232c4055a838
Christopher Erick Moody <chrisemoody@gmail.com>
2019-08-21 10:03:15 -0700

    Update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 url = 'https://github.com/cemoody/flexi_hash_embedding'
 
 install_requires = ['torch', 'torch-scatter', 'sklearn']
